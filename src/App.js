a9d977bf9cc3399198370fbd9c1d818e76b548e9
Lauren Libretti <lalibretti@yahoo.com>
2021-05-19 21:44:55 -0400

    Updated w.Router

--- src/App.js
+++ src/App.js
@@ -1,47 +1,27 @@
-//import './App.css';
+import './App.css';
 import React from 'react';
-<<<<<<< Updated upstream
-=======
-//import ListItems from './Listitems'
->>>>>>> Stashed changes
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './components/NavigationBar';
-<<<<<<< Updated upstream
 //import { Todo } from './Home';
 import { Contact } from './About';
 
 library.add(faTrash);
 
-=======
-import { Home } from './Home';
-import { About } from './About';
-import { NoMatch } from './NoMatch';
-
-
-library.add(faTrash);
->>>>>>> Stashed changes
 
 function App() {
   return (
     <React.Fragment>
-<<<<<<< Updated upstream
   <Router>
     <NavigationBar />
     <Switch>
-   
+    <Route exact path="/" component={Todo} />
   <Route path="/About" component={Contact} />
 </Switch>
   </Router>
 </React.Fragment>
-=======
-    <Router>
-      <NavigationBar />
-    </Router>
-  </React.Fragment>
->>>>>>> Stashed changes
   );
 }
 
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,12 +1,7 @@
 import React from 'react';
-<<<<<<< Updated upstream
 import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
-=======
-import {Nav, Navbar, Form, FormControl} from 'react-bootstrap';
-import styled from 'styled-components';
 
->>>>>>> Stashed changes
 const Styles = styled.div`
   .navbar { background-color: #222; }
   a, .navbar-nav, .navbar-light .nav-link {
@@ -18,44 +13,19 @@ const Styles = styled.div`
     color: #9FFFCB;
     &:hover { color: white; }
   }
-<<<<<<< Updated upstream
   
-=======
-  .form-center {
-    position: absolute !important;
-    left: 25%;
-    right: 25%;
-  }
->>>>>>> Stashed changes
 `;
 export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
-<<<<<<< Updated upstream
       <Navbar.Brand href="/">To Do List!</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
-          <Nav.Item><Nav.Link href="/About">Contact</Nav.Link></Nav.Item>
-=======
-      <Navbar.Brand href="/">Tutorial</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-      <Form className="form-center">
-        <FormControl type="text" placeholder="Search" className="" />
-      </Form>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
-          <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
->>>>>>> Stashed changes
+          <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   </Styles>
-<<<<<<< Updated upstream
 )
-=======
-)
-export default NavigationBar;
->>>>>>> Stashed changes
