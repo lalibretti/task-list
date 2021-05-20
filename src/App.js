38bb61c02df9c722d7001608425e78c592ccfcd1
Lauren Libretti <lalibretti@yahoo.com>
2021-05-19 21:30:14 -0400

    NavBar/Contact Form
    Working navbar & contact form forked from main branch

--- src/App.js
+++ src/App.js
@@ -1,87 +1,28 @@
 import './App.css';
 import React from 'react';
-import ListItems from './Listitems'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+//import { Todo } from './Home';
+import { Contact } from './About';
 
 library.add(faTrash);
 
-class App extends React.Component {
-  constructor (props){
-    super(props);
-    this.state={
-      items:[],
-      currentItem:{
-        text:'',
-        key:''
-      }
-    }
-    this.addItem =this.addItem.bind(this);
-    this.handleInput =this.handleInput.bind(this);
-    this.deleteItem =this.deleteItem.bind(this);
-    this.setUpdate =this.setUpdate.bind(this);
-  }
 
-  addItem(e){
-    e.preventDefault();
-    const newItem = this.state.currentItem;
-    console.log(newItem);
-    if(newItem.text !==''){
-      const items = [...this.state.items, newItem];
-      this.setState ({
-        items: items,
-        currentItem:{
-          text:'',
-          key:''
-        }
-      })
-    }
-  }
-
-  handleInput(e){
-    this.setState({
-      currentItem:{
-        text:e.target.value,
-        key: Date.now()
-      }
-    })
-  }
-
-  deleteItem(key){
-    const filteredItems = this.state.items.filter(item => 
-      item.key !==key);
-      this.setState({
-        items:filteredItems
-      })
-  }
-  setUpdate (text, key) {
-    console.log('items:'+this.state.items);
-    const items = this.state.items;
-    items.map(item => {
-      if(item.key===key){
-        console.log(item.key+''+key)
-        item.text= text;
-      }
-    })
-    this.setState({
-      items:items
-    })
-  }
-
-  render() {
-    return ( 
-      <div className = 'App'>
-        <header>
-          <form id = "to-do-form"onSubmit={this.addItem}>
-            <input type = 'text' placeholder = "Enter items to do!" value={this.state.currentItem.text} onChange={this.handleInput}/> 
-              <button type = 'submit'> Add item </button> 
-          </form> 
-          <p>{this.state.text}</p>
-        <ListItems items={this.state.items}   deleteItem = {this.deleteItem} setUpdate ={this.setUpdate} />
-        </header> 
-      </div>
-    );
-  }
+function App() {
+  return (
+    <React.Fragment>
+  <Router>
+    <NavigationBar />
+    <Switch>
+   
+  <Route path="/About" component={Contact} />
+</Switch>
+  </Router>
+</React.Fragment>
+  );
 }
 
 export default App;
