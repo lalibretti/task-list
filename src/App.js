abb2279824bd678d2357e45f674a4359d5fafcda
Lauren Libretti <lalibretti@yahoo.com>
2021-05-20 16:37:09 -0400

    contact form broken :/

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './components/NavigationBar';
-//import { Todo } from './Home';
+import { Todo } from './Home';
 import { Contact } from './About';
 
 library.add(faTrash);
